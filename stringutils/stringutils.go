490e9bf3a806c2f7ec08b83d43faaea2e9ccf6b8
Rafael de Aquino Cunha <aquinocunha@gmail.com>
2018-07-18 10:46:48 -0300

    Adicionada funções de criptografia.

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -1,6 +1,13 @@
 // O pacote Dreambridge_utils contém funções úteis para várias coisas diversas
 package stringutils
 
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 // Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
 	r := []rune(s)
@@ -8,4 +15,24 @@ func Reverse(s string) string {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
+}
+
+// CriptografaString - Criptografa uma senha
+func CriptografaString(senha string, cost int) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(senha), cost)
+
+	fmt.Println("Hash: " + string(bytes))
+	return string(bytes), err
+}
+
+// checkStringHash - Verifica se a criptografia corresponde à string passada
+func checkStringHash(str, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(str))
+
+	if err != nil {
+		log.Println("checkStringHash - Erro ao validar string.")
+		return false, err
+	}
+
+	return true, nil
 }
